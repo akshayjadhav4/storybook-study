802dfd8b4c676b6d4b3a434e0d4611745ca7d19f
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-12 19:40:33 +0530

    style improved

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -6,6 +6,6 @@ export default {
   component: Input,
 };
 
-export const large = () => <Input variant="large" />;
-export const medium = () => <Input variant="medium" />;
-export const small = () => <Input variant="small" />;
+export const large = () => <Input variant="large" placeholder="Large" />;
+export const medium = () => <Input variant="medium" placeholder="Medium" />;
+export const small = () => <Input variant="small" placeholder="Small" />;
